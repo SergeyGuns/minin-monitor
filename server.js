0537cc6a0cf4bf64da73a957ad2b71b955eddbc3
Sergey Ganin <mcheguevara2@gmail.com>
2017-07-19 20:26:37 +0300

    fix

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ let timeuotTime = 5000;
 var express = require('express');
 var app = express();
 
-app.use('/static', express.static(__dirname + '/build'));
+app.use('/', express.static(__dirname + '/build'));
 
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('Example app listening on port 80!');
 });
 
